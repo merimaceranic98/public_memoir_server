8cc16ab5f15672ffbf7433c2a1b20eee67f93cfe
merimaceranic98 <merima.ceranic@ministryofprogramming.com>
2022-09-22 13:33:21 +0200

    Added CORS

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+app.use(cors());
 
 server.use(middlewares);
 server.use(router);
